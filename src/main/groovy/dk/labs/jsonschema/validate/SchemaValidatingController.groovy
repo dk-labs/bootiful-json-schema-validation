bee3146fc1fb493d9dfc0bf6695a3991963147ab
Dmitriy Kopylenko <dima767@gmail.com>
2018-10-12 12:16:36 -0400

    Refactor

--- src/main/groovy/dk/labs/jsonschema/validate/SchemaValidatingController.groovy
+++ src/main/groovy/dk/labs/jsonschema/validate/SchemaValidatingController.groovy
@@ -1,7 +1,13 @@
 package dk.labs.jsonschema.validate
 
-
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.fge.jsonschema.core.report.LogLevel
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jsonschema.main.JsonSchema
+import com.github.fge.jsonschema.main.JsonSchemaFactory
 import mjson.Json
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.MethodParameter
 import org.springframework.core.io.Resource
@@ -13,7 +19,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter
@@ -25,11 +30,15 @@ import java.lang.reflect.Type
  */
 @RestController
 @ControllerAdvice
-@RequestMapping('/validatedjson')
 class SchemaValidatingController extends RequestBodyAdviceAdapter {
 
     @Value('classpath:basic-schema.json')
     Resource jsonSchemaResource
+
+    @Autowired
+    ObjectMapper jacksonMapper
+
+    static String VALIDATE_WITH_MJSON = 'validateWithMJson'
 
     @Override
     boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -41,26 +50,45 @@ class SchemaValidatingController extends RequestBodyAdviceAdapter {
                          MethodParameter parameter,
                          Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
 
-        Json.Schema schema = Json.schema(jsonSchemaResource.getURI())
-
-        Json incomingJson = Json.make(body)
-
-        def validationResult = schema.validate(incomingJson)
-        if(!validationResult.at('ok')) {
-            throw new JsonSchemaValidationFailedException(validationResult.at('errors').asList())
-        }
+        parameter.getMethod().name == VALIDATE_WITH_MJSON ?
+                validateWithMjsonLibrary(body) :
+                validateWithJsonSchemaValidatorLibrary(body)
 
         return super.afterBodyRead(body, inputMessage, parameter, targetType, converterType)
     }
 
-    @PostMapping
-    ResponseEntity<?> validateJson(@RequestBody Map<String, Object> incomingJsonMap) {
+    @PostMapping("/jsonvalidate1")
+    ResponseEntity<?> validateWithMJson(@RequestBody Map<String, Object> incomingJsonMap) {
+        return ResponseEntity.ok().build()
+    }
+
+    @PostMapping("/jsonvalidate2")
+    ResponseEntity<?> validateWithJsonSchemaValidator(@RequestBody Map<String, Object> incomingJsonMap) {
         return ResponseEntity.ok().build()
     }
 
     @ExceptionHandler(JsonSchemaValidationFailedException)
     final ResponseEntity<?> handleUserNotFoundException(JsonSchemaValidationFailedException ex, WebRequest request) {
         new ResponseEntity<>([errors: ex.errors], HttpStatus.BAD_REQUEST)
+    }
+
+    private validateWithMjsonLibrary(requestBody) {
+        Json incomingJson = Json.make(requestBody)
+        Json.Schema schema = Json.schema(this.jsonSchemaResource.getURI())
+        def validationResult = schema.validate(incomingJson)
+        if (!validationResult.at('ok')) {
+            throw new JsonSchemaValidationFailedException(validationResult.at('errors').asList())
+        }
+    }
+
+    private validateWithJsonSchemaValidatorLibrary(requestBody) {
+        JsonNode incomingJson = jacksonMapper.valueToTree(requestBody)
+        JsonSchema schema = JsonSchemaFactory.byDefault().getJsonSchema(this.jsonSchemaResource.getURI().toString())
+        ProcessingReport validationReport = schema.validate(incomingJson)
+        if(!validationReport.success) {
+            def errorMsgs = validationReport.findAll {it.logLevel == LogLevel.ERROR}.collect {it.message}
+            throw new JsonSchemaValidationFailedException(errorMsgs)
+        }
     }
 
 }
